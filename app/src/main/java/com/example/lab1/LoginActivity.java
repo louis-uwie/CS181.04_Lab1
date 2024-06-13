bbfdc4fb37e3db050a352d5be605a011c2edaaa9
uwie <louis.binwag@student.ateneo.edu>
2024-06-13 09:37:05 +0800

    Added register redirect. Added register initial layout.

--- app/src/main/java/com/example/lab1/LoginActivity.java
+++ app/src/main/java/com/example/lab1/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.lab1;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -45,6 +47,13 @@ public class LoginActivity extends AppCompatActivity {
 
 
 
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register();
+            }
+        });
+
     }
 
 
@@ -54,8 +63,9 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void register(){
+        Intent registerActivity = new Intent(this, RegisterActivity.class);
 
-
+        startActivity(registerActivity);
     }
 
     public void clear(){
