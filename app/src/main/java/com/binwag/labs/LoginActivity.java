75bdac39403d9a12ce262ed6da9f03d4de8286ff
uwie <157711065+louis-uwie@users.noreply.github.com>
2024-07-01 16:03:18 +0800

    Update LoginActivity.java
    Fixed UI

--- app/src/main/java/com/binwag/labs/LoginActivity.java
+++ app/src/main/java/com/binwag/labs/LoginActivity.java
@@ -9,6 +9,9 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import io.realm.Realm;
 
@@ -24,6 +27,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         realm = Realm.getDefaultInstance();
         myAccounts = getSharedPreferences("myAccounts", MODE_PRIVATE);
@@ -35,7 +43,6 @@ public class LoginActivity extends AppCompatActivity {
         clearButton = findViewById(R.id.btnClear);
         rememberMe = findViewById(R.id.cbRememberMe);
 
-        // Load saved credentials if "Remember Me" is checked
         if (myAccounts.getBoolean("rememberMe", false)) {
             String savedUsername = myAccounts.getString("username", "");
             String savedPassword = myAccounts.getString("password", "");
