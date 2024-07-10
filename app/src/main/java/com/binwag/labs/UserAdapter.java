3b23a9f7c51e37a5a8761f1a4ecb4d1db5f0327c
uwie <louis.binwag@student.ateneo.edu>
2024-07-10 18:07:17 +0800

    Camera can now be called.
    TODO:
    
    Display photo taken in profile.

--- app/src/main/java/com/binwag/labs/UserAdapter.java
+++ app/src/main/java/com/binwag/labs/UserAdapter.java
@@ -6,10 +6,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.MemoryPolicy;
+import com.squareup.picasso.NetworkPolicy;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
 
 import io.realm.OrderedRealmCollection;
 import io.realm.RealmRecyclerViewAdapter;
@@ -17,10 +24,9 @@ import io.realm.RealmRecyclerViewAdapter;
 public class UserAdapter extends RealmRecyclerViewAdapter<User, UserAdapter.UserViewHolder> {
 
     public class UserViewHolder extends RecyclerView.ViewHolder{
-        TextView nameTextView;
-        TextView passwordTextView;
-        Button deleteButton;
-        Button editButton;
+        TextView nameTextView, passwordTextView;
+        Button deleteButton, editButton;
+        ImageView imageView;
 
         public UserViewHolder(@NonNull View itemView){
             super(itemView);
@@ -30,12 +36,12 @@ public class UserAdapter extends RealmRecyclerViewAdapter<User, UserAdapter.User
 
             deleteButton = itemView.findViewById(R.id.btnDelete);
             editButton = itemView.findViewById(R.id.btnEdit);
+
+            imageView = itemView.findViewById(R.id.imageView);
         }
     }
 
-
     UserManagement activity;
-
 
     public UserAdapter(UserManagement activity, OrderedRealmCollection<User> data) {
         super(data, true);
@@ -81,6 +87,32 @@ public class UserAdapter extends RealmRecyclerViewAdapter<User, UserAdapter.User
                     activity.startActivity(editIntent);
                 }
             });
+
+            holder.imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    activity.takePhoto(user);
+                }
+            });
+
+            File getImageDir = activity.getExternalCacheDir();
+
+            // just a sample, normally you have a diff image name each time
+            if (user.getImageUrl()!=null) {
+                File file = new File(getImageDir, user.getImageUrl());
+
+                if (file.exists()) {
+                    // this will put the image saved to the file system to the imageview
+                    Picasso.get()
+                            .load(file)
+                            .networkPolicy(NetworkPolicy.NO_CACHE)
+                            .memoryPolicy(MemoryPolicy.NO_CACHE)
+                            .into(holder.imageView);
+                } else {
+                    // use a default picture
+                    holder.imageView.setImageResource(R.mipmap.ic_launcher);
+                }
+            }
         }
     }
 
